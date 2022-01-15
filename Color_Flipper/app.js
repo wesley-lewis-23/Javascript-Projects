60b64209d4d0863fe34faa77754c1ec89a43b302
wesley-lewis <89732422+wesley-lewis@users.noreply.github.com>
2022-01-15 17:28:04 +0530

    Add files via upload

---
+++ Color_Flipper/app.js
@@ -0,0 +1,15 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+  //Get random number between 0 and 3
+  const randomNumber = Math.floor(getRandomNumber() * colors.length);
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.random();
+}
